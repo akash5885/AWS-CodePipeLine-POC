e59b4f46dfe5d58eb023513c61c251b86c1d37f5
Deval Sheth <Deval.sheth@einfochips.com>
2018-07-17 14:02:50 +0530

    AWS Code pipeline POC

---
+++ src/main/java/com/eic/aws/poc/aws/codepipeline/poc/LoginController.java
@@ -0,0 +1,22 @@
+package com.eic.aws.poc.aws.codepipeline.poc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class LoginController {
+	@RequestMapping(value="/login", method = RequestMethod.POST)
+	public String login(ModelMap model, @RequestParam String name){
+		model.put("name", name);
+		return "welcome";
+	}
+	
+	@RequestMapping(value="/", method = RequestMethod.GET)
+	public String showIndexPage(){
+		return "index";
+	}
+}
